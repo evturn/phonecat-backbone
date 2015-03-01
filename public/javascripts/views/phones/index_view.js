c9a71bbcdf228274c9a782bc1ddb94fa84659a4f
Evan Turner <evturn@gmail.com>
2015-03-01 07:19:21 -0500

    hard coding in a phone collection as the initialize function

--- public/javascripts/views/phones/index_view.js
+++ public/javascripts/views/phones/index_view.js
@@ -1,6 +1,20 @@
 PhonesIndexView = Backbone.View.extend({
 	initialize: function() {
+		this.collection = new PhonesCollection([
+			{'name': 'Nexus S',
+				'snippet': 'Fast just got fast as shit.'},
+			{'name': 'Motorola Plastic Thing with Wi-Fi',
+			'snippet': 'Drop me. It should fine.'},
+			{'name': 'Motorola Plastic Thing',
+			'snippet': 'Drop me. It should fine.'}
+		])
+
 		this.render();
+
+		new PhonesIndexViewListView({
+			el: this.$('ul.phones'),
+			collection: this.collection
+		});
 	},
 	render: function() {
 		this.$el.html(JST['phones/index']());
